220445d764a86bc6e062e187baa4fa22c6a2c6b4
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2025-09-20 23:45:54 +0900

    Changes to be committed: 기억안나 	modified:   src/main/java/com/rrayy/EB/bill/bill_event.java

--- src/main/java/com/rrayy/EB/bill/bill_event.java
+++ src/main/java/com/rrayy/EB/bill/bill_event.java
@@ -1,8 +1,11 @@
 package com.rrayy.EB.bill;
 
 import org.bukkit.event.Listener;
-import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class bill_event implements Listener {
-    
+    public void onPlayerInteraction(PlayerInteractEvent event) {
+        ItemStack item = event.getItem();
+    }
 }
