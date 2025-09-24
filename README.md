7f2cfed9e11d60ceba230517c5d33d173c82d985
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2025-09-24 19:16:17 +0900

    Changes to be committed: bill item 클릭 시 XP 주기 	modified:   README.md 	modified:   src/main/java/com/rrayy/EB/bill/bill_event.java

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ### TODO-List
 - [x]  bill item 만들기
-- [ ]  bill item 클릭 시 XP 주기
+- [x]  bill item 클릭 시 XP 주기
 - [x]  bill item으로 바꾸는 명령어 만들기
 - [ ]  번역기능 만들기
--- src/main/java/com/rrayy/EB/bill/bill_event.java
+++ src/main/java/com/rrayy/EB/bill/bill_event.java
@@ -4,8 +4,25 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
+import com.rrayy.EB.EB;
+
 public class bill_event implements Listener {
+    private final EB plugin;
+
+    public bill_event(EB plugin){
+        this.plugin = plugin;
+    }
+
     public void onPlayerInteraction(PlayerInteractEvent event) {
         ItemStack item = event.getItem();
+        if (item == null) return;
+        bill_item bill = new bill_item(plugin, 0);
+        if (bill.is_bill(item)) {
+            bill.set_item(item);
+            event.getPlayer().setLevel(event.getPlayer().getLevel() + bill.level);
+            event.getPlayer().sendMessage(plugin.PREFIX+"경험치 지폐를 사용하여 "+bill.level+"만큼의 경험치를 얻었습니다.");
+            item.setAmount(item.getAmount() - 1); // 지폐 1장 소모
+            event.setCancelled(true); // 기본 동작 취소
+        }
     }
 }
